034e2be362dcadeda175573d808b3bf22e0e462f
Nasreddine <nasreddine.bensalha@ensi-uma.tn>
2022-09-28 00:46:26 +0200

    Add OpenAPI config

--- src/main/java/com/example/bank/configuration/OpenApiConfig.java
+++ src/main/java/com/example/bank/configuration/OpenApiConfig.java
@@ -5,10 +5,28 @@
  */
 package com.example.bank.configuration;
 
-//@Configuration
-//@EnableWebMvc
-public class SwaggerConfig {
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                        .title("Bank service API")
+                        .description("This is a sample bank service demo")
+                        .termsOfService("terms")
+                        .license(new License().name("GNU"))
+                        .version("1.0")
+                );
+    }
 }
-
-
