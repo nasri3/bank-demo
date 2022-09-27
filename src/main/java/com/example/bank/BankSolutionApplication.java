821ff192cb1ee41cf76bdd911ed9d9f73654f618
Nasreddine <nasreddine.bensalha@talium.fr>
2022-09-27 11:19:39 +0200

    Configuration swagger

---
+++ src/main/java/com/example/bank/BankSolutionApplication.java
@@ -0,0 +1,20 @@
+package com.example.bank;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EntityScan("com.example.bank.model")
+@EnableJpaRepositories({"com.example.bank.dao"})
+public class BankSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSolutionApplication.class, args);
+	}
+}
